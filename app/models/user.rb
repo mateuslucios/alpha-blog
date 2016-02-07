9e5f1f941965355a36adf2ebcc7ed0ba4ab557ca
mateuslucios <mateus.lucios@gmail.com>
2016-02-07 15:56:09 -0200

    user-article association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   has_many :articles
 
+  before_save { self.email = email.downcase }
+
   validates :username, presence: true, 
             length: {minimum: 3, maximum: 25}, 
             uniqueness: {case_sensitive: false}
